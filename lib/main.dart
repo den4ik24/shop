b856c6e5f73f441ac17d9947524cc13504c6e5cd
Denis Yurchenko <den4ik24@gmail.com>
2023-02-16 22:56:13 +0300

    again on VSCode_adding_authScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import './screens/cart_screen.dart';
-import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
@@ -10,6 +9,7 @@ import './providers/orders.dart';
 import "./screens/orders_screen.dart";
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
               .copyWith(secondary: Colors.orange),
           fontFamily: "Lato",
         ),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routName: (ctx) => CartScreen(),
