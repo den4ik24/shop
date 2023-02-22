6b371c13e6c422a0aea84ef86a587b7e2a58a8a7
Denis Yurchenko <den4ik24@gmail.com>
2023-02-22 15:44:04 +0300

    login&signUp

---
+++ lib/providers/auth.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  late String _token;
+  late DateTime _expiryDate;
+  late String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA7Z8W7BKNiIFHuvNttJ5_kTo_sCbDj3o4');
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          "email": email,
+          "password": password,
+          "returnSecureToken": true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
+  }
+}
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth.dart';
 
 enum AuthMode { Signup, Login }
 
@@ -59,7 +62,10 @@ class AuthScreen extends StatelessWidget {
                       child: Text(
                         'MyShop',
                         style: TextStyle(
-                          color: Theme.of(context).primaryTextTheme.titleMedium!.color,
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .titleMedium!
+                              .color,
                           fontSize: 50,
                           fontFamily: 'Anton',
                           fontWeight: FontWeight.normal,
@@ -100,7 +106,7 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  void _submit() {
+  Future <void> _submit() async {
     if (!_formKey.currentState!.validate()) {
       // Invalid!
       return;
@@ -111,8 +117,16 @@ class _AuthCardState extends State<AuthCard> {
     });
     if (_authMode == AuthMode.Login) {
       // Log user in
+       await Provider.of<Auth>(context, listen: false).login(
+        _authData["email"]!,
+        _authData["password"]!,
+      );
     } else {
       // Sign user up
+      await Provider.of<Auth>(context, listen: false).signup(
+        _authData["email"]!,
+        _authData["password"]!,
+      );
     }
     setState(() {
       _isLoading = false;
